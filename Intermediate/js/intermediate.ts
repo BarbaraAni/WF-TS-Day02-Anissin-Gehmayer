9aa5eec209a44052e03b9c52dab8d2125b482eb2
Barbara Anissin <anissinb@gmail.com>
2019-10-30 14:48:12 +0100

    orbit start

---
+++ Intermediate/js/intermediate.ts
@@ -0,0 +1,3 @@
+let orbit: Array <number> = [1, 0.2408467, 0.61519726, 1.8808158, 11.862615, 29.447498, 84.016846, 164.79132]
+let planets: Array <string> = [Earth, Mercury, Venus, Mars, Jupiter, Saturn, Uranus, Neptune]
+
